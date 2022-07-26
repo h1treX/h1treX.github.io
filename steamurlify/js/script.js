b93d533d41758286e306664849a37a4839fee160
h1treX <dieblauejacke@web.de>
2022-07-26 13:11:20 +0200

    changed resources structure

--- steamurlify/js/script.js
+++ steamurlify/js/script.js
@@ -1,39 +1,47 @@
 const options = {
-    creatorTwitter: "h1treXCS",
-    versionInfo: {
-        number: "1.0.1",
-        lastUpdated: new Date("Mon Jul 25 2022"),
-    },
+  creatorTwitter: "h1treXCS", versionInfo: {
+    number: "1.0.1", lastUpdated: new Date("Mon Jul 25 2022"),
+  },
 }
 let steamUrl;
 
-const twitterLink = document.getElementById("twitter");
-twitterLink.innerText = options.creatorTwitter;
-twitterLink.href = "https://twitter.com/" + options.creatorTwitter;
+function init() {
+  const twitterLink = document.getElementById("twitter");
+  twitterLink.innerText = options.creatorTwitter;
+  twitterLink.href = "https://twitter.com/" + options.creatorTwitter;
 
-const versionInfo = document.getElementById("version-info");
-versionInfo.innerText = `v${options.versionInfo.number} (Last updated: ${options.versionInfo.lastUpdated.toDateString()})`
+  const versionInfo = document.getElementById("version-info");
+  versionInfo.innerText = `v${options.versionInfo.number} (Last updated: ${options.versionInfo.lastUpdated.toDateString()})`
 
-const ipInput = document.getElementById("ip-input"), pwInput = document.getElementById("pw-input");
+  const ipInput = document.getElementById("ip-input"), pwInput = document.getElementById("pw-input");
 
-const inputFunction = () => {
+  const inputFunction = () => {
+    const buttonOutput = document.getElementById("button-output");
     if (ipInput.value) {
-        const pw = pwInput.value;
 
-        steamUrl = `steam://connect/${ipInput.value}`;
-        if (pw) {
-            steamUrl += `/${pw}`
-        }
+      const pw = pwInput.value;
+      steamUrl = `steam://connect/${ipInput.value}`;
+      if (pw) {
+        steamUrl += `/${pw}`
 
-        //add url to href of a element
-        const buttonOutput = document.getElementById("button-output");
-        buttonOutput.href = steamUrl;
-        buttonOutput.classList.remove("d");
+      }
+      //add url to href of a element
+      buttonOutput.href = steamUrl;
+      buttonOutput.classList.remove("d");
     } else {
-        document.getElementById("button-output").classList.add("d");
-        steamUrl = null;
+      buttonOutput.classList.add("d");
+      buttonOutput.href = null;
+
+      steamUrl = null;
     }
+  }
+
+  ipInput.onchange = inputFunction
+  pwInput.onchange = inputFunction
 }
 
-ipInput.onchange = inputFunction
-pwInput.onchange = inputFunction
+init();
+
+addEventListener("fetch", event => {
+  return
+})
